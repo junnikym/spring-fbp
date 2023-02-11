4af97f2a1e0577735897692c8489b67c3854a60a
junnikym <junnikym@gmail.com>
2023-02-11 20:34:55 +0900

    initialize project

---
+++ src/main/kotlin/org/junnikym/springfbp/SpringFbpApplication.kt
@@ -0,0 +1,11 @@
+package org.junnikym.springfbp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringFbpApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringFbpApplication>(*args)
+}
---
+++ src/test/kotlin/org/junnikym/springfbp/SpringFbpApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.junnikym.springfbp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringFbpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
