d423a4ed7fbe5e1cf06c774458c2cec0fafc74d4
junnikym <junnikym@gmail.com>
2023-02-12 22:10:24 +0900

    add: functions about root nodes in bean dependency link factory

--- bean-manager/src/main/kotlin/org/junnikym/springfbp/BeanDependencyLinkFactory.kt
+++ bean-manager/src/main/kotlin/org/junnikym/springfbp/BeanDependencyLinkFactory.kt
@@ -6,6 +6,20 @@ interface BeanDependencyLinkFactory {
 
     fun add(link: List<BeanDependencyLink>);
 
+
+
+    fun hasParent(nodeName: String): Boolean;
+
+    fun hasFromNodeParent(link: BeanDependencyLink): Boolean;
+
+    fun isRoot(beanName: String): Boolean;
+
+    fun getRootLinks(): List<BeanDependencyLink>;
+
+    fun getRootNames(): List<String>;
+
+
+
     fun getLinks(): ArrayList<BeanDependencyLink>;
 
     fun getLinks(name: String): ArrayList<BeanDependencyLink>;
