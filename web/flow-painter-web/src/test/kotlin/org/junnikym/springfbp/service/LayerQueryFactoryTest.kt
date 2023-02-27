24695fe1d35507c23e01c464c139fd0ab17c728b
junnikym <junnikym@gmail.com>
2023-02-28 00:07:23 +0900

    add: BeanLayerFactory unit test

--- web/flow-painter-web/src/test/kotlin/org/junnikym/springfbp/service/LayerQueryFactoryTest.kt
+++ web/flow-painter-web/src/test/kotlin/org/junnikym/springfbp/service/LayerQueryFactoryTest.kt
@@ -1,13 +1,11 @@
 package org.junnikym.springfbp.service
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junnikym.springfbp.beans.TestBeanDependencyLinkFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
 class LayerQueryFactoryTest(
@@ -23,6 +21,28 @@ class LayerQueryFactoryTest(
             .javaClass
             .constructors[0]
             .newInstance(linkFactory) as BeanLayerFactory;
+
+        this.beanLayerFactory.update()
     }
+
+    @Test
+    fun get_bean_layers() {
+        // given
+
+        // when
+        val rootLayer = this.beanLayerFactory.get("testRootBean");
+        val internalALayer = this.beanLayerFactory.get("testInternalBeanA");
+        val internalBLayer = this.beanLayerFactory.get("testInternalBeanB");
+        val internalCLayer = this.beanLayerFactory.get("testInternalBeanC");
+        val leafLayer = this.beanLayerFactory.get("testLeafBean");
+
+        // then
+        assertEquals(rootLayer, 0)
+        assertEquals(internalALayer, 1)
+        assertEquals(internalBLayer, 1)
+        assertEquals(internalCLayer, 1)
+        assertEquals(leafLayer, 2)
+    }
+
 
 }
