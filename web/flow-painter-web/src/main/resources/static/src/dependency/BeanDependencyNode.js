6615309bddca887264460eb2e2a45c77214857d5
junnikym <junnikym@gmail.com>
2023-03-13 17:58:55 +0900

    edit: positioning link lines

--- web/flow-painter-web/src/main/resources/static/src/dependency/BeanDependencyNode.js
+++ web/flow-painter-web/src/main/resources/static/src/dependency/BeanDependencyNode.js
@@ -1,6 +1,8 @@
 class BeanDependencyNode {
 
-    static DEFAULT_HEIGHT = 360
+    static nodes = new Map()
+
+    static DEFAULT_HEIGHT = 120
     static DEFAULT_WIDTH  = 240
 
     constructor(
@@ -11,14 +13,21 @@ class BeanDependencyNode {
     ) {
         this.x = x; this.y = y
         this.width = width; this.height= height
-
+        this.center = {
+            x: this.x + (width/2),
+            y: this.y + (height/2)
+        }
+        this.centerInGroup = {
+            x: width/2,
+            y: height/2,
+        }
         this.dom = dom
-        this.bodyDom = dom.getElementsByClassName("bean-dependency-node.body")[0]
-        this.linkDoms = [...dom.getElementsByClassName("bean-dependency-link-with")]
 
-        console.log(this.linkDoms)
+        this.bodyDom = dom.getElementsByClassName("bean-dependency-node.body")[0]
+        this.linkDoms = [...dom.getElementsByClassName("bean-dependency-link.line")]
 
         this.init()
+        BeanDependencyNode.nodes.set(dom.id, this)
     }
 
     init() {
@@ -31,5 +40,26 @@ class BeanDependencyNode {
     applySize() {
         this.bodyDom.setAttribute("width", this.width)
         this.bodyDom.setAttribute("height", this.height)
+    }
+
+    updateLines() {
+        if(this.linkDoms.length === 0)
+            return;
+
+        const group = this.dom.getElementsByClassName("bean-dependency-link")[0]
+        group.setAttribute(
+            'transform',
+            `translate(${this.centerInGroup.x}, ${this.centerInGroup.y})`
+        )
+
+        this.linkDoms.forEach(line=> {
+            const targetId = line.getAttribute("value")
+            if(!BeanDependencyNode.nodes.has(targetId))
+                return
+
+            const target = BeanDependencyNode.nodes.get(targetId)
+            line.setAttribute("x2", target.center.x-this.center.x)
+            line.setAttribute("y2", target.center.y-this.center.y)
+        })
     }
 }
