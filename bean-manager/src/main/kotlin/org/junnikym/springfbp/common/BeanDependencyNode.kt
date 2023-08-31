e5d4e011a9f557368b6cdf3520a132fc7c49c0a1
junnikym <junnikym@gmail.com>
2023-08-31 10:35:35 +0900

    add: override equals&hashcode method in Link, Node class

--- bean-manager/src/main/kotlin/org/junnikym/springfbp/common/BeanDependencyNode.kt
+++ bean-manager/src/main/kotlin/org/junnikym/springfbp/common/BeanDependencyNode.kt
@@ -19,4 +19,19 @@ class BeanDependencyNode (name: String, clazz: Class<*>, bean: Any? = null) {
         return name
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BeanDependencyNode
+
+        if (name == name) return true
+        if (clazz == other.clazz) return true
+        return bean == bean
+    }
+
+    override fun hashCode(): Int {
+        return 31 * clazz.hashCode() + bean.hashCode() + name.hashCode()
+    }
+
 }
